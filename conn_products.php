842a8ac95b544896a0e0874f7a2159b6cc4cee7c
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2021-12-16 14:00:35 +0300

    pro_php_4

---
+++ conn_products.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "gb.com";
+$username = "developer-evgeniy";
+$password = "403)(nlr)ivJk8(4";
+$dbname = "php6";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT title,price,description FROM products limit 5";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo $row["title"]. " | цена: " . $row["price"]. "<br>". "описание: ". $row["description"].  "<br><br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+
