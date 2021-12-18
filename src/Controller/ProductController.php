7c0f31e376190ae96c4a56e40839e8225d82a590
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2021-12-18 17:33:42 +0300

    php8

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index(): Response
+    {
+        $manager = $this->getDoctrine()->getManager();
+        $repository = $manager->getRepository(Product::class);
+        $products =$repository->findAll();
+       
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
+
