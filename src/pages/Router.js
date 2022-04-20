36e512f1ae41efc4e4a43b2c18f2ec462b715f1f
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2022-04-20 21:57:01 +0300

    react_les_7

---
+++ src/pages/Router.js
@@ -0,0 +1,30 @@
+  import {
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
+  import Home from '../pages/Home';
+  import Profile from '../pages/Profile';
+  import Chats from '../components/Chats';
+  import React from 'react';
+ 
+const Router = () => {
+
+    return (
+    <>
+    <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/profile'>Profile</Link></li>
+        <li><Link to='/chats'>Chats</Link></li>
+    </ul>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+        <Route path="/profile" element={<Profile />}/>
+        
+<Route path="/chats/:chatId" element={<Chats/>} />
+        <Route path="*" element={<Chats/>} />
+    </Routes>
+    </>
+    );
+};
+export default Router;
