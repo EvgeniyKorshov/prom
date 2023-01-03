bb9c5a8ad61f41e7e9555ad28869180c81dacc16
EvgeniyKorshov <80601543+EvgeniyKorshov@users.noreply.github.com>
2023-01-03 11:58:34 +0300

    react

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.scss'
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <div className="footer container">
+        <p className={"footer_text"}>2022 - Дипломный проект “Клавиатурный тренажер”</p>
+      </div>
+
+    </footer>
+  );
+};
+
+export default Footer
